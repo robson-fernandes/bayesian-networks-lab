ac9e1151a3bf44942c27645a63269219f8b5fdb7
Robson Fernandes <robson.ux@gmail.com>
2018-05-15 00:57:24 -0300

    Bayesian Networks

--- init.r
+++ init.r
@@ -1,6 +1,13 @@
-# init.R
-#
-# Example R code to install packages if not already installed
-#
+#  my_packages = c("forecast")
 
-install.packages("forecast", dependencies = TRUE)
+# ###########################################################
+
+# install_if_missing = function(p) {
+#   if (p %in% rownames(installed.packages()) == FALSE) {
+#     install.packages(p)
+#   }
+#   else {
+#     cat(paste("Skipping already installed package:", p, "\n"))
+#   }
+# }
+# invisible(sapply(my_packages, install_if_missing))
